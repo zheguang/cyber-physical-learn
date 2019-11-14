1d68ea4da3cad4044e7def727c118d9c1336b7a0
Zheguang Zhao <zheguang.zhao@gmail.com>
2019-11-14 11:35:35 -0700

    hybrid automata

---
+++ src/HybridAutomata.jl
@@ -0,0 +1,179 @@
+using JuMP, Ipopt, LinearAlgebra
+
+# case 1: 1-bit logical control, 1 transition condition, 1 process (done)
+# case 2: 2-bit logical control (ind), 1 transition condition (1 proc-dep), 2 processes (ind) (done)
+# case 3: 2-bit logical control (ind), 1 transition codnition (1 proc-dep), 2 processes (rel)
+# case 4: 2-bit logical control (rel), 1 transition condition (1 proc-dep), 2 processes (ind)
+# case 5: 2-bit logical control (rel), 1 transition condition (1 proc-dep), 2 processes (rel)
+
+# case 6: 1-bit logical control, 2 transition conditions, 1 process
+
+# For transition conditions
+# h_1: 1 transition condition for 1-bit control,  based on single process each
+# h_1_mp: 1 transition condition for 1-bit control, based on multiple processes
+# h_2: 2 transition condition for 2-bit control, based on single process each
+
+
+function f(A, B, y, u)
+    A * y + B * u
+end
+
+
+function example_f_case_3()
+    # 2-bit control (ind), 2 processes (ind)
+    A = Diagonal([0.9; 0.85])
+    B = Diagonal([4; 5])
+    y = [20; 20]
+    u = [0; 0]
+    A, B, y, u
+end
+
+
+function example_f_case_4()
+    # 2-bit control (rel), 2 processes (ind)
+    A = Diagonal([0.9; 0.85])
+    B = [4  1;
+         2  5]
+    y = [20; 20]
+    u = [0; 0]
+    A, B, y, u
+end
+
+
+function example_f_case_5()
+    # 2-bit control (rel), 2 processes (rel)
+    A = [0.9  0.3;
+         0.2  0.85]
+    B = [4  1;
+         2  5]
+    y = [20; 20]
+    u = [0; 0]
+    A, B, y, u
+end
+
+function H_1(y, u, C, D)
+    u_next = (u + ([(1 .- u)'; u']' * (C * [y'; y'] + D) .< 0)' ) .% 2
+    return u_next
+end
+
+
+
+function h_1(y, u)
+    # 1-bit control transition, each with single process dependence
+    u′ = zeros(size(u))
+
+    # heater: 1st bit
+    b = 1
+    if u[b] == 0
+        if y[b] < 20  # y[b] - 20 < 0
+            u′[b] = 1
+        else
+            u′[b] = u[b]
+        end
+    else
+        # u == 1
+        if y[b] > 25 # 25 - y[b] < 0
+            u′[b] = 0
+        else
+            u′[b] = u[b]
+        end
+    end
+    u′
+end
+
+
+function h_2(y, u)
+    # 2-bit control transition, each with singel process dependence
+    u′ = zeros(size(u))
+
+    # heater: 1st bit
+    b = 1
+    if u[b] == 0
+        if y[b] < 20
+            u′[b] = 1
+        else
+            u′[b] = u[b]
+        end
+    else
+        # u == 1
+        if y[b] > 25
+            u′[b] = 0
+        else
+            u′[b] = u[b]
+        end
+    end
+
+    # heater: 2nd bit
+    b = 2
+    if u[b] == 0
+        if y[b] < 20
+            u′[b] = 1
+        else
+            u′[b] = u[b]
+        end
+    else
+        # u == 1
+        if y[b] > 25
+            u′[b] = 0
+        else
+            u′[b] = u[b]
+        end
+    end
+
+    u′
+end
+
+
+function trajectory(T, A, B, y₀, u₀, h)
+    (y, u) = (y₀, u₀)
+    n_y = size(y)[1]
+    n_u = size(u)[1]
+    Y = zeros(n_y, T)
+    U = zeros(n_y, T)
+    for t = 1:T
+        u = h(y, u)
+        y = f(A, B, y, u)
+        U[:, t] = u
+        Y[:, t] = y
+    end
+    (Y, U)
+end
+
+
+function ols(Y_out, Y_in, U_in)
+    @assert size(Y_out)[1] == size(Y_in)[1]
+    @assert size(Y_out)[2] == size(Y_in)[2] == size(U_in)[2]
+
+    n_y, n_t = size(Y_in)
+    n_u = size(U_in)[1]
+    @show n_y, n_t, n_u
+
+    model = Model(with_optimizer(Ipopt.Optimizer))
+
+    @variable(model, Â[1:n_y, 1:n_y])
+    @variable(model, B̂[1:n_y, 1:n_u])
+    @objective(model, Min, sum((Y_out - (Â * Y_in + B̂ * U_in)) .^ 2))
+
+    optimize!(model)
+    @show value.(Â)
+    @show value.(B̂)
+
+    value.(Â), value.(B̂)
+end
+
+
+function format_ols(Y, U)
+    Y_in = Y[:, 1:end-1]
+    Y_out = Y[:, 2:end]
+    U_in = U[:, 2:end]  # u(t) is first updated with y(t-1) and then used to calculate y(t)
+
+    Y_out, Y_in, U_in
+end
+
+
+function example()
+    A, B, y, u = example_f_case_3()
+    Y, U = trajectory(100, A, B, y, u, h_2)
+    Y_out, Y_in, U_in = format_ols(Y, U)
+    ols(Y_out, Y_in, U_in)
+end
--- src/RoomTemperature.jl
+++ src/RoomTemperature.jl
@@ -1,10 +1,6 @@
 module RoomTemperature
 
-using DataFrames
-
-T_min = 24.0
-T_max = 26.0
-T_Δ = 26 * 3
+using DataFrames, Gadfly
 
 # Euler's method for 1st order approximation
 L(x; dy, x_0, y_0) = y_0 + dy(x_0, y_0) * (x - x_0)
@@ -15,10 +11,12 @@ L(x; dy, x_0, y_0) = y_0 + dy(x_0, y_0) * (x - x_0)
 # a: decay factor
 # T_Δ: heater capacity
 # u: heater switch; 0 for OFF and 1 for ON
-dT(t, T; a = 1, u) = -a * T + T_Δ * u
+# OFF: dT = a * (T - T_ext)
+# ON: dT = a * (T - T_ext + T_Δ / a)
+dT(t, T; u, T_Δ, T_ext, a) = a * T + T_Δ * u - a * T_ext
 
 # Transition function for heater thermostat automaton
-function δ(u, T)
+function δ(u, T; T_min, T_max)
     if T < T_min
         return 1
     elseif T > T_max
@@ -31,16 +29,16 @@ end
 
 # Approxiamtion for room temperature given initial condition
 # Outputs temperature T and control u at time t, given previous state.
-function system(t; u_0, t_0, T_0)
+function system(t; u_0, t_0, T_0, T_min, T_max, T_Δ, T_ext, a)
     # physical at time t
-    T_t = L(t; dy = (t_0, T_0) -> dT(t_0, T_0; u = u_0), x_0 = t_0, y_0 = T_0)
+    T_t = L(t; dy = (t_0, T_0) -> dT(t_0, T_0; u = u_0, T_Δ=T_Δ, T_ext=T_ext, a=a), x_0 = t_0, y_0 = T_0)
     # cyber at time t
-    u_t = δ(u_0, T_t)
+    u_t = δ(u_0, T_t; T_min=T_min, T_max=T_max)
     return (u_0, T_0, u_t, T_t)
 end
 
 
-function simulate(t_max; Δt = 1e-2)
+function simulate(t_max; Δt = 1e-1, T_min=19.0, T_max=21.0, T_Δ=5, T_ext=17, a=-1)
     u_ini, t_ini, T_ini = 0, 0.0, 20.0
 
     u = u_ini
@@ -56,11 +54,23 @@ function simulate(t_max; Δt = 1e-2)
 
     # simulate from t_ini to t_max for each time step Δt
     for (i, t) in enumerate(t_ini:Δt:t_max-Δt)
-        (u_0, T_0, u, T) = system(t + Δt; u_0 = u, t_0 = t, T_0 = T)
+        (u_0, T_0, u, T) = system(t + Δt; u_0=u, t_0=t, T_0=T, T_min=T_min, T_max=T_max, T_Δ=T_Δ, T_ext=T_ext, a=a)
         R[i, :] = [t u_0 T_0 u T u - u_0]
     end
 
     return DataFrame(R, names)
 end
+
+function plotSample(t_max; Δt=1e-1, T_min=19.0, T_max=21.0, T_Δ=5, T_ext=17, a=-1)
+    df = simulate(t_max; Δt=Δt, T_min=T_min, T_max=T_max, T_Δ=T_Δ, T_ext=T_ext, a=a)
+    df[!, :u] = df[!, :u] .== 0
+    plot(df, x=:t, y=:T, color=:u,
+         Geom.point,
+         Guide.title("Temperature Trajectory of Thermostat"),
+         Guide.xlabel("Time"),
+         Guide.ylabel("Temperature"),
+         Guide.colorkey(title="Heater", labels=["Off", "On"]))
+end
+
 
 end # module RoomTemperature
